4bebee235db67c891f75bba287d22551fc339019
renan <renancunhafonseca@gmail.com>
2020-05-18 20:09:47 -0300

    neural network backpropagation finished

---
+++ deep_learning/feed_forward/layers.py
@@ -0,0 +1,46 @@
+import numpy as np
+from typing import Tuple
+
+
+class LinearLayer:
+
+    def __init__(self, shape: Tuple[int, int]):
+        self.weights = np.random.randn(shape[0], shape[1])
+        self.biases = np.zeros((shape[1], 1))
+
+    def forward(self, x: np.ndarray):
+        self.x = x
+        self.next_x = (self.x @ self.weights) + self.biases.T
+        return self.next_x
+
+    def backward(self, next_grad_inputs: np.ndarray):
+        self.grad_weights = self.x.T @ next_grad_inputs
+        self.grad_biases = np.sum(next_grad_inputs, axis=0, keepdims=True).T
+        return next_grad_inputs @ self.weights.T
+
+
+class SqrErrorLayer:
+
+    def __init__(self, y: np.ndarray):
+        self.y = y
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        self.x = x
+        self.next_x = np.square(x-self.y)
+        return self.next_x
+
+    def backward(self, next_grad_inputs: np.ndarray) -> np.ndarray:
+        grad_inputs = -(self.y - self.x)*2*next_grad_inputs
+        return grad_inputs
+
+
+class SigmoidLayer:
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        self.next_x = 1/(np.exp(-x)+1)
+        return self.next_x
+
+    def backward(self, next_grad_inputs: np.ndarray) -> np.ndarray:
+        return (self.next_x*(1-self.next_x))*next_grad_inputs
+
+
--- deep_learning/feed_forward/neural_network.py
+++ deep_learning/feed_forward/neural_network.py
@@ -3,6 +3,7 @@ from typing import List
 from sklearn.metrics import mean_squared_error
 from abc import ABC, abstractmethod
 from typing import Tuple, List
+from layers import LinearLayer, SigmoidLayer, SqrErrorLayer
 
 
 class NeuralNetwork:
@@ -11,96 +12,56 @@ class NeuralNetwork:
         self.learning_rate = 0.1
         self.training_iters = 1
         self.layers = []
-        for index, neuron in enumerate(num_neurons[:-1]):
+        for index, neuron in enumerate(num_neurons[:-2]):
+            print(neuron, num_neurons[index+1])
             layer = LinearLayer((neuron, num_neurons[index+1]))
             self.layers.append(layer)
+            layer = SigmoidLayer()
+            self.layers.append(layer)
+        layer = LinearLayer((num_neurons[-2], num_neurons[-1]))
+        self.layers.append(layer)
+        self.loss_layer = None
+    
+    def backward_propagation(self, output: np.ndarray) -> None:
+        grad_input = self.loss_layer.backward(np.ones_like(output)) 
+        for layer_index in range(len(self.layers)-1, -1, -1):
+            grad_input = self.layers[layer_index].backward(grad_input)
+
+    def update_params(self, learning_rate: float) -> None:
+        for layer_index in range(len(self.layers)):
+            layer = self.layers[layer_index]
+            if type(layer) == LinearLayer:
+                self.layers[layer_index].weights -= learning_rate*layer.grad_weights
+                self.layers[layer_index].biases -= learning_rate*layer.grad_biases
+                
 
     def fit(self, x: np.ndarray, y: np.ndarray,
-            learning_rate: float = 0.0001,
-            training_iters: int = 1000) -> np.array:
-
-        x = np.insert(x, 0, 1, 1)
-
-        loss_layer = SqrErrorLayer(y)
-        self.layers.append(loss_layer)
+            learning_rate: float, training_iters: int) -> np.array:
+        self.loss_layer = SqrErrorLayer(y)
 
         losses = np.empty(self.training_iters)
-        for i in range(self.training_iters):
-            print(f"y: {y}")
-            print(f"x: {x}")
+        for i in range(training_iters):
 
-            
-            grad_inputs = self.layers[-1].backward()
-            #for layer in
-            z2 = self.linear_layer.forward(x)
-            print(f"x+1: {z2}")
-            loss = self.loss.forward(z2)
-            print(f"loss: {loss}")
-            d2 = self.loss.backward()
-            print(f"loss backward: {d2}")
-            d3 = self.linear_layer.backward(d2)
-            print(f" linear backward: {d3}")
-            gradient = self.linear_layer.grad_weights
-            gradient *= self.learning_rate
-
-            self.linear_layer.weights -= gradient
+            y_pred = self.predict(x)
+            error = self.loss_layer.forward(y_pred)
+            mean_error = np.mean(error)
+            self.backward_propagation(y_pred)
+                   
+            print(f"EPOCH {i}")
+            print(f"MSE Error: {mean_error}")
+            self.update_params(learning_rate)
 
         return losses
 
     def predict(self, x: np.ndarray) -> np.ndarray:
-        x = np.insert(x, 0, 1, 1)
         for layer in self.layers:
             x = layer.forward(x)
         return x
 
-
-class LinearLayer:
-
-    def __init__(self, shape: Tuple[int, int]):
-        self.weights = np.random.randn(shape[0], shape[1])
-
-    def forward(self, x: np.ndarray):
-        self.x = x
-        self.next_x = np.dot(x, self.weights)
-        return self.next_x
-
-    def backward(self, next_grad_inputs: np.ndarray):
-        self.grad_weights = np.dot(self.x.T,
-                                   next_grad_inputs) / next_grad_inputs.shape[0]
-        grad_inputs = next_grad_inputs.dot(self.weights.transpose())
-        return grad_inputs
-
-
-class SqrErrorLayer:
-
-    def __init__(self, y: np.ndarray):
-        self.y = y
-
-    def forward(self, x: np.ndarray) -> np.ndarray:
-        self.x = x
-        self.next_x = np.mean(np.square(x-self.y), axis=0, keepdims=True)
-        return self.next_x
-
-    def backward(self) -> np.ndarray:
-        grad_inputs = -1*(self.y - self.x)
-        return grad_inputs
-
-
-class SigmoidLayer:
-
-    def forward(self, x: np.ndarray) -> np.ndarray:
-        self.next_x = 1/(np.exp(-x)+1)
-        return self.next_x
-
-    def backward(self, next_grad_inputs: np.ndarray) -> np.ndarray:
-        return (self.next_x.T*(1-self.next_x).T)*next_grad_inputs
-        
-
 if __name__ == "__main__":
-    np.random.seed(0)
-    model = NeuralNetwork(training_iters=1, learning_rate=0.01)
-    x = np.array([[-10]])
+    model = NeuralNetwork([1, 10, 1])
+    x = np.random.randn(10).reshape(-1, 1)
     y = 5 + 3*x + np.random.normal(0, 3)
-    errors = model.fit(x, y)
-    print(y)
-    print(model.predict(x)[:10])
+    print(x.shape)
+    print(y.shape)
+    model.fit(x, y, training_iters=1000, learning_rate=0.001)
